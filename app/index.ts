83b2375e457046b2b05e835935ba97f78d58f7cf
Justin Henricks <justinhenricks@gmail.com>
2023-10-21 22:20:23 -0400

    server started

---
+++ app/index.ts
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import express from "express";
+import { PORT } from "./contants";
+
+async function main() {
+  const app = express();
+
+  app.get("/", async (req, res) => {
+    res.send("Hello World!");
+  });
+
+  app.listen(PORT, () => {
+    console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  });
+}
+
+main();
