15a06614189026a628851f1caea646f14e06bb7a
Justin Henricks <justinhenricks@gmail.com>
2023-10-22 23:31:15 -0400

    Create google-calendar.service.ts

---
+++ app/services/google/google-calendar.service.ts
@@ -0,0 +1,16 @@
+import { googleCalendarClient } from "./index";
+
+export const listEvents = async (calendarId: string) => {
+  try {
+    const response = await googleCalendarClient.events.list({
+      calendarId,
+      timeMin: new Date().toISOString(),
+      maxResults: 300,
+      singleEvents: true,
+      orderBy: "startTime",
+    });
+    return response.data.items;
+  } catch (error) {
+    throw error;
+  }
+};
