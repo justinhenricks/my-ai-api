150d98a70549929b3e8ceb56a84694dd42a96565
Justin Henricks <justinhenricks@gmail.com>
2023-10-22 23:18:51 -0400

    Create api-error.ts

---
+++ app/utils/api-error.ts
@@ -0,0 +1,28 @@
+export class ApiError extends Error {
+  statusCode: number;
+  errors?: any[];
+
+  constructor(statusCode: number, message: string, errors?: any[]) {
+    super(message);
+    this.statusCode = statusCode;
+    this.errors = errors;
+  }
+
+  static badRequest(error: unknown, errors?: any[]): ApiError {
+    console.log(error);
+    if (error instanceof Error) {
+      return new ApiError(400, error.message, errors);
+    }
+    return new ApiError(400, "Unknown error", errors);
+  }
+
+  static notFound(message: string, errors?: any[]) {
+    return new ApiError(404, message, errors);
+  }
+
+  static unauthorized(message: string, errors?: any[]) {
+    return new ApiError(401, message, errors);
+  }
+
+  // Add more static methods as needed for other HTTP status codes
+}
