f327108d8e246451ced8830bff461ff415fb1f8e
Justin Henricks <justinhenricks@gmail.com>
2023-10-23 11:33:16 -0400

    Schedule the embeddings builder

--- app/crons/runner.ts
+++ app/crons/runner.ts
@@ -1,7 +1,16 @@
 import cron from "node-cron";
 import { constructGigsJson } from "./construct-gigs-json.cron";
+import { doEmbeddings } from "./do-embeddings";
 
 /**
  * Upsert gigs.json from google calendar events once an hour
  */
 cron.schedule("0 * * * *", constructGigsJson);
+
+// For testing, once every 15 seconds:
+// cron.schedule("*/15 * * * * *", doEmbeddings);
+
+// cron.schedule("*/15 * * * * *", doEmbeddings);
+
+// Run the `doEmbeddings` function every 6 hours
+cron.schedule("0 0,6,12,18 * * *", doEmbeddings);
