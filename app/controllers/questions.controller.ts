2782a4369213749071970c1917a2ab807abbf6e2
Justin Henricks <justinhenricks@gmail.com>
2023-10-23 13:57:58 -0400

    remove console

--- app/controllers/questions.controller.ts
+++ app/controllers/questions.controller.ts
@@ -7,7 +7,6 @@ import { ApiError } from "../utils/api-error";
 export class QuestionsController {
   static async answerQuestion(req: Request, res: Response, next: NextFunction) {
     try {
-      console.log("req.body", req.body);
       const { question } = req.body;
 
       const template = `Use the following pieces of context to answer the question at the end. If you come across dates/times to use in your answer, please format them and use a 12-hour clock. If you don't know the answer, just say that you don't know, don't try to make up an answer. After saying you don't know, you can say, "I will take my best guess" and then give your best guess. Use three sentences maximum and keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer.
