3801e37b5309efb91a3113d0a37197cf63cf8985
Justin Henricks <justinhenricks@gmail.com>
2023-11-08 21:07:40 -0500

    v2 calculating prev and long term EMAs now

--- app/index.ts
+++ app/index.ts
@@ -14,8 +14,6 @@ async function main() {
   // Create a WebSocket client instance
   const ws = new WebSocket(`${GEMINI_PUBLIC_WS_BASE_URL}/v2/marketdata/BTCUSD`);
 
-  let lastClose = 0;
-
   ws.on("open", function open() {
     console.log("Connected to the WebSocket server!");
 
@@ -29,101 +27,106 @@ async function main() {
     ws.send(JSON.stringify(subscribeMessage));
   });
 
-  const periodShort = 5; // Short EMA period
-  const periodLong = 10; // Long EMA period
-  let prices = []; // Stores the recent prices
-  let EMA_short = 0;
-  let EMA_long = 0;
-  let initialized = false; // This will be true once we have enough data to calculate the SMA
+  // Configuration for short-term and long-term EMA periods
+  const shortTermPeriod = 2; // e.g., 120 minutes for 2-hour EMA
+  const longTermPeriod = 6; // e.g., 360 minutes for 6-hour EMA
 
-  // Function to calculate SMA
-  function calculateSMA(prices, period) {
-    const sum = prices.reduce((acc, val) => acc + val, 0);
+  // State to keep track of previous EMA values
+  let prevShortTermEma: undefined | number = undefined;
+  let prevLongTermEma: undefined | number = undefined;
+
+  // Price history - we only need to keep 'longTermPeriod' number of latest prices
+  let priceHistory: number[] = [];
+
+  function onNewPrice(newPrice: number) {
+    // Add the new price to the history
+    console.log("ok in new price, here is price history", priceHistory);
+    priceHistory.push(newPrice);
+
+    // Ensure we don't keep more prices than necessary
+    if (priceHistory.length > longTermPeriod) {
+      priceHistory.shift(); // Remove the oldest price
+    }
+
+    // Calculate the short-term EMA if we have enough data points
+    if (priceHistory.length >= shortTermPeriod) {
+      let newShortTermEma = calculateEMA(
+        priceHistory.slice(-shortTermPeriod),
+        shortTermPeriod,
+        prevShortTermEma
+      );
+      if (newShortTermEma !== undefined) {
+        prevShortTermEma = newShortTermEma;
+        console.log(`New short-term EMA is: ${newShortTermEma}`);
+      }
+    }
+
+    // Calculate the long-term EMA if we have enough data points
+    if (priceHistory.length >= longTermPeriod) {
+      let newLongTermEma = calculateEMA(
+        priceHistory,
+        longTermPeriod,
+        prevLongTermEma
+      );
+      if (newLongTermEma !== undefined) {
+        prevLongTermEma = newLongTermEma;
+        console.log(`New long-term EMA is: ${newLongTermEma}`);
+      }
+    }
+
+    // Check if we should make a trade (EMA crossover logic)
+    if (prevShortTermEma !== undefined && prevLongTermEma !== undefined) {
+      // This is where you'd check for a crossover and decide whether to trade
+      if (prevShortTermEma > prevLongTermEma) {
+        // Signal for bullish crossover, potentially a buy signal
+      } else if (prevShortTermEma < prevLongTermEma) {
+        // Signal for bearish crossover, potentially a sell signal
+      }
+    }
+  }
+
+  function calculateSMA(prices: number[], period: number) {
+    let sum = prices.slice(-period).reduce((acc, val) => acc + val, 0);
     return sum / period;
   }
 
-  // Function to calculate EMA
   function calculateEMA(
-    previousEMA,
-    price,
-    multiplier,
-    period,
-    isFirstCalculation = false
+    prices: number[],
+    period: number,
+    prevEma: number | undefined
   ) {
-    if (isFirstCalculation) {
-      // If this is the first calculation, use the SMA as the previous EMA
-      return calculateSMA(prices.slice(-period), period);
-    } else {
-      return (price - previousEMA) * multiplier + previousEMA;
+    // The smoothing constant
+    const k = 2 / (period + 1);
+
+    // If we don't have a previous EMA, calculate the SMA for the first EMA value
+    // This assumes we have at least `period` number of prices
+    if (prevEma === undefined && prices.length >= period) {
+      prevEma = calculateSMA(prices, period);
     }
+
+    // If we still don't have a previous EMA (due to not enough data), return undefined
+    if (prevEma === undefined) {
+      return undefined;
+    }
+
+    // The latest price is the most recent price in our prices array
+    const latestPrice = prices[prices.length - 1];
+
+    // Calculate the EMA based on the previous EMA and the latest price
+    let newEma = (latestPrice - prevEma) * k + prevEma;
+
+    return newEma;
   }
 
   ws.on("message", function incoming(data) {
     // Parse the incoming data
     const message = JSON.parse(data);
-    console.log("here is message", message);
+
     if (message.type === "candles_1m_updates") {
       const candle = message.changes[0];
       const [, , , , close] = candle;
-      prices.push(close); // Store the closing price for EMA calculation
 
-      console.log("ok here are prices", prices);
-
-      // Ensure we have enough data to calculate the initial SMA
-      if (!initialized && prices.length >= periodLong) {
-        initialized = true;
-        // Calculate initial EMA values using SMA
-        EMA_short = calculateEMA(
-          EMA_short,
-          close,
-          2 / (periodShort + 1),
-          periodShort,
-          true
-        );
-        EMA_long = calculateEMA(
-          EMA_long,
-          close,
-          2 / (periodLong + 1),
-          periodLong,
-          true
-        );
-      } else if (initialized) {
-        // Update EMAs with the latest closing price
-        EMA_short = calculateEMA(
-          EMA_short,
-          close,
-          2 / (periodShort + 1),
-          periodShort
-        );
-        EMA_long = calculateEMA(
-          EMA_long,
-          close,
-          2 / (periodLong + 1),
-          periodLong
-        );
-      }
-
-      // Print the EMAs to the console
-      if (initialized) {
-        console.log(`Short EMA: ${EMA_short}`);
-        console.log(`Long EMA: ${EMA_long}`);
-      }
-
-      // Check if we can issue a buy/sell signal based on the EMAs
-      if (initialized && EMA_short > EMA_long) {
-        // Signal a potential buy
-        const buyPrice = close;
-        console.log(`Buy signal at: ${buyPrice}`);
-        // Simulate a sell order at 1% higher
-        const sellPrice = buyPrice * 1.01;
-        console.log(`Sell order placed at: ${sellPrice}`);
-      }
-
-      // Keep the prices array from growing indefinitely
-      if (prices.length > periodLong * 2) {
-        // Keeping twice the long period worth of prices for accuracy
-        prices.shift(); // Remove the oldest price
-      }
+      onNewPrice(close);
     }
   });
 
