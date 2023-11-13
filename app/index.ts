7e6b945cbdeceec1326ae01351ca5d5fa6aadd1c
Justin Henricks <justinhenricks@gmail.com>
2023-11-12 22:30:15 -0500

    init my bot in main

--- app/index.ts
+++ app/index.ts
@@ -1,54 +1,19 @@
 import "dotenv/config";
 import express from "express";
-import WebSocket from "ws";
 import { IS_PROD, PORT } from "./constants";
 import "./crons/runner"; // This schedules the cron jobs
 import { router } from "./routes";
-import { GeminiSocket } from "./web-sockets/gemini-socket";
+import { MarketWatcher } from "./trading/market-watcher";
+import { OrderWatcher } from "./trading/order-watcher";
 async function main() {
   const app = express();
 
   app.use(express.json());
   app.use(router);
 
-  // const marketDataSocket = new MarketDataWebSocket(
-  //   `${GEMINI_PUBLIC_WS_BASE_URL}/v2/marketdata/BTCUSD`
-  // );
-
-  // const orderEventSocket = new OrderEventsWebSocket(
-  //   `wss://api.gemini.com/v1/order/events`
-  // );
-
-  const geminiPublicSocket = new GeminiSocket({
-    endpoint: "/v2/marketdata/BTCUSD",
-    messageHandler: (data: WebSocket.Data) => {
-      const message = JSON.parse(data.toString());
-
-      if (message.type === "candles_1m_updates") {
-        const candle = message.changes[0];
-        const [, , , , close] = candle;
-
-        console.log("here is close", close);
-      }
-    },
-    api: "public",
-    subscriptions: [
-      {
-        type: "subscribe",
-        subscriptions: [{ name: "candles_1m", symbols: ["BTCUSD"] }],
-      },
-    ],
-  });
-
-  // const orderEventsSocket = new GeminiSocket({
-  //   endpoint: "/v1/order/events",
-  //   messageHandler: (data: WebSocket.Data) => {
-  //     const message = JSON.parse(data.toString());
-
-  //     console.log("here is order event message,", message);
-  //   },
-  //   api: "private",
-  // });
+  //Init Crpyto Bot
+  const marketWatcher = new MarketWatcher();
+  const orderWatcher = new OrderWatcher();
 
   app.listen(PORT, () => {
     console.log(
